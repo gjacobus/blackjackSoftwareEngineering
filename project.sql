1aae618bd57e05314b359e443d91db833924db43
gjacobus <riovista449@comcast.net>
2018-12-03 17:47:18 -0600

    add sql file

---
+++ project.sql
@@ -0,0 +1,19 @@
+drop table BlackJackData;
+
+create table BlackJackData
+	(username varchar(20) not null,
+	password varchar(20) not null,
+	Balance INTEGER);
+alter table BlackJackData
+	add constraint username_pk primary key(username);
+	
+insert into BlackJackData values('gjacobus', aes_encrypt('hello', 'blackjack'), 50);
+
+insert into BlackJackData values('mdodd', aes_encrypt('hello123', 'blackjack'), 50);
+
+insert into BlackJackData values('msmith', aes_encrypt('hello123', 'blackjack'), 1000);
+
+insert into BlackJackData values('frank', aes_encrypt('hello123', 'blackjack'), 50);
+
+
+	
